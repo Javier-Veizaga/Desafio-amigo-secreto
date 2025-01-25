4ffe70435f6071b5e46138f6bec29bb1d08260f1
Veizaga <veizagamoralesjose@gmail.com>
2025-01-25 16:31:58 -0400

    funciones

--- app.js
+++ app.js
@@ -3,40 +3,40 @@
 let amigos = [];
 
 function agregarAmigo() {
-
-    
     let input = document.getElementById('amigo');
 
     let nombre = input.value.trim();
     
-    
     if (nombre === "") {
-
         alert("Por favor, inserte un nombre.");
         return;
     }
     
-    amigos.push(nombre)
+    amigos.push(nombre);
 
-    actualizarLista()
+    actualizarLista();
 
     input.value = "";
 }
+
 function actualizarLista() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = "";
 
     // Generar los elementos de la lista
-        amigos.forEach((amigo) => {
-
+    amigos.forEach((amigo) => {
         let li = document.createElement('li');
         li.textContent = amigo;
         lista.appendChild(li);
-        
-        });
+    });
 }
 
 function sortearAmigo() {
+    // Verificar si hay integrantes en la lista
+    if (amigos.length === 0) {
+        alert("No hay integrantes en la lista. Por favor, añade al menos uno.");
+        return;
+    }
     
     let numRandomico = Math.floor(Math.random() * amigos.length);
     console.log(`El número random es: ${numRandomico}`);
@@ -46,15 +46,16 @@ function sortearAmigo() {
 
     const newItem = document.createElement('li');
 
-    limpiarAmigos()
+    limpiarAmigos();
 
     newItem.textContent = `El seleccionado es: ${amigos[numRandomico]}`;
     
     resultList.appendChild(newItem);
 
 }
+
 function limpiarAmigos() {
-    //Apunte a listaAmigos por id, y realice la limpieza.
+    // Apuntar al elemento listaAmigos por ID y realizar la limpieza
     const listaAmigos = document.getElementById("listaAmigos");
     listaAmigos.innerHTML = "";
 
